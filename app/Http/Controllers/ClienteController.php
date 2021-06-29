b29e45c855f22bbd1e156689e5ca2b4c5a5c91a9
LuricDev <lricardosouzaribeiro@gmail.com>
2021-06-29 19:54:09 -0400

    Configurando o ClienteController adicionando a função store para cadastrar o cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -12,4 +12,26 @@ class ClienteController extends Controller
     {
         return view('clientes.create');
     }
+
+    // recebe as informações do formulário e as grava
+    // no banco de dados
+    public function store(Request $request)
+    {
+        // valida o formulário
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'telefone' => 'required',
+            'celular' => 'required',
+            'senha' => 'required'
+        ]);
+
+        // obtém os valores do form
+        Cliente::create($request->all());
+
+        // direciona para página cadastro novamente,
+        // com uma mensagem de sucesso
+        return redirect()->route('clientes.create')
+            ->with('mensagem', 'Cliente salvo com sucesso.');
+    }
 }
