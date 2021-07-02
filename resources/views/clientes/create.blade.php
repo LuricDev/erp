72752da981c8a9e02e26a5d32d1266468bac643c
LuricDev <lricardosouzaribeiro@gmail.com>
2021-07-01 21:51:46 -0400

    Modificando o layout do create na view Clientes

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,48 +1,61 @@
 @extends('layout')
 
 @section('content')
-<h1>Novo Cliente</h1>
+<style>
+    .container {
+        max-width: 450px;
+    }
 
-@if($status = Session::get('mensagem'))
-<h2>{{ $status }}</h2>
-@endif
+    .push-top {
+        margin-top: 50px;
+    }
+</style>
 
-@if($errors->any())
-<h2>Houve alguns erros ao processar o formulário</h2>
-<ul>
-    @foreach($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
+<div class="card push-top">
+    <div class="card-header">
+        Novo Cliente
+    </div>
 
-<form action="{{ route('clientes.store') }}" method="post">
-    @csrf
-    <table width="200" cellspacing="3" cellpadding="3">
-        <tr>
-            <td>Nome:</td>
-            <td><input type="text" name="nome" id="nome" placeholder="Nome"></td>
-        </tr>
-        <tr>
-            <td>E-mail:</td>
-            <td><input type="text" name="email" id="email" placeholder="E-mail"></td>
-        </tr>
-        <tr>
-            <td>Telefone</td>
-            <td><input type="text" name="telefone" id="telefone" placeholder="Telefone"></td>
-        </tr>
-        <tr>
-            <td>Celular</td>
-            <td><input type="text" name="celular" id="celular" placeholder="Celular"></td>
-        </tr>
-        <tr>
-            <td>Senha</td>
-            <td><input type="text" name="senha" id="senha" placeholder="Senha"></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><button type="submit">Cadastrar</button></td>
-        </tr>
-    </table>
-</form>
+    @if($status = Session::get('mensagem'))
+        <div class="alert alert-sucess">{{ $status }}</div>
+    @endif
+
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <h2>Houve alguns erros ao processar o formulário</h2>
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="card-body">
+        <form action="{{ route('clientes.store') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" name="nome"/>
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" name="email"/>
+            </div>
+            <div class="form-group">
+                <label for="telefone">Telefone</label>
+                <input type="tel" class="form-control" name="telefone"/>
+            </div>
+            <div class="form-group">
+                <label for="celular">Celular</label>
+                <input type="tel" class="form-control" name="celular"/>
+            </div>
+            <div class="form-group">
+                <label for="senha">Senha</label>
+                <input type="text" class="form-control" name="senha"/>
+            </div>
+            <button type="submit" class="btn btn-block btn-danger">Cadastrar Cliente</button>
+        </form>
+    </div>
+</div>
 @endsection
